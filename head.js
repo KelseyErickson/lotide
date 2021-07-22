6d34d472a9439c90214a598c19c9ce09e18face8
Kelsey Erickson <kerickson@gmx.com>
2021-07-22 01:16:09 +0000

    Adding file head.js

---
+++ head.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if (array[0]) {
+    return array[0];
+  } else {
+    return undefined;
+  }
+};
+
+// TEST CODE 
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5,6,7]), 6);
+assertEqual(head([8]), 8);
+assertEqual(head([]), 'Nothing');
+assertEqual(head(['head']), 'head');
