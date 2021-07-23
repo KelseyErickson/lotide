9879b84cf00c6443c8b5a9e983a7a799dc23e029
Kelsey Erickson <kerickson@gmx.com>
2021-07-23 23:30:39 +0000

    Adding eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let outcome = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      outcome = false;
+    }
+  }
+  return outcome;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 4, 3], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 4, 3], [1, 2, '3']), true); // => should FAIL
+assertEqual(eqArrays(['hello', 'goodbye', false, 27], ['hello', 'goodbye', false, 27]), true); // => should PASS
+assertEqual(eqArrays([], []), true); // => should PASS
