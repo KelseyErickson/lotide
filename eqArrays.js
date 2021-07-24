d47e6fe364dfb3824403fea28032fd6d2d020cec
Kelsey Erickson <kerickson@gmx.com>
2021-07-24 21:58:02 +0000

    Added comments about the function

--- eqArrays.js
+++ eqArrays.js
@@ -6,6 +6,8 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Function to check if two arrays are equal 
+
 const eqArrays = function(array1, array2) {
   let outcome = true;
   for (let i = 0; i < array1.length; i++) {
