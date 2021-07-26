aff5a9438ab29e3af17527aa9400ce1573a76300
Kelsey Erickson <kerickson@gmx.com>
2021-07-26 23:49:29 +0000

    Added and if statement to check for Array Length

--- eqArrays.js
+++ eqArrays.js
@@ -6,13 +6,15 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// Function to check if two arrays are equal 
+// Function to check if two arrays are equal
 
 const eqArrays = function(array1, array2) {
   let outcome = true;
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      outcome = false;
+    if (array1.length === array2.length) {
+      if (array1[i] !== array2[i]) {
+        outcome = false;
+      }
     }
   }
   return outcome;
