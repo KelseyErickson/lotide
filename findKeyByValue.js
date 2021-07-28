42f5dff51f0a79db3ff76585146d3e0e9147401f
Kelsey Erickson <kerickson@gmx.com>
2021-07-28 21:39:55 +0000

    Added initial test code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -14,3 +14,12 @@ const findKeyByValue = function(object, value) {
 
 
 };
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
