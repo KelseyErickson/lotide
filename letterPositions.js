b3f84daf0ab015a33d7c39953602c92581367a79
Kelsey Erickson <kerickson@gmx.com>
2021-07-28 20:19:35 +0000

    Added eqArrays and assertArraysEqual functions

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,28 @@
+const eqArrays = function(array1, array2) {
+  let outcome = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1.length === array2.length) {
+      if (array1[i] !== array2[i]) {
+        outcome = false;
+      }
+    } else {
+      outcome = false;
+    }
+  }
+  return outcome;
+};
+
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else if (eqArrays(array1, array2) === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
 const letterPositions = function(sentence) {
   const results = {};
   
