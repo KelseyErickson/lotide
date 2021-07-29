e7fd3c71d21b9fd11b52182723bdece7732bb755
Kelsey Erickson <kerickson@gmx.com>
2021-07-29 21:37:58 +0000

    Added more test code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -39,6 +39,7 @@ const eqObjects = function (object1, object2) {
 // Function to take two objects and log a message to assert if the objects are equal
 
 const assertObjectsEqual = function(actual, expected) {
+  
   if (eqObjects(actual, expected) === true) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (eqArrays(actual, expected) === false) {
@@ -50,4 +51,19 @@ const assertObjectsEqual = function(actual, expected) {
 console.log('ab vs ba:')
 const ab = { a: '1', b: '2' };
 const ba = { b: '2', a: '1' };
-assertObjectsEqual(ab,ba);
+assertObjectsEqual(ab, ba);
+
+console.log('ab vs abc:')
+const abc = { a: '1', b: '2', c: '3' };
+assertObjectsEqual(ab, abc);
+
+console.log('cd vs dc:')
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc);
+
+console.log('cd vs cd2:')
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2);
+
+
