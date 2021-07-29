c216bceec149c869244b7c5c81e864e479c9870e
Kelsey Erickson <kerickson@gmx.com>
2021-07-29 20:48:32 +0000

    Added comments

--- eqObjects.js
+++ eqObjects.js
@@ -28,15 +28,15 @@ const eqObjects = function (object1, object2) {
   const keysObject2 = Object.keys(object2);
 
 
-  if (!(keysObject1.length === keysObject2.length)) {
+  if (!(keysObject1.length === keysObject2.length)) { // If the length of the keys of the objects do not equal then return false
     return false;
   }
-  for (const key of keysObject1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
+  for (const key of keysObject1) { // for every key in the key objects array
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // if both key values are arrays
+      if (!eqArrays(object1[key], object2[key])) { // if the eqArrays find them to be false - return false
         return false;
       }
-    } else if (object1[key] !== object2[key]) {
+    } else if (object1[key] !== object2[key]) { // else if the key values are not arrays then compare the primitive values
       return false;
     }
   }
@@ -44,18 +44,24 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
+// Test 1
 console.log('ab vs ba:')
 const ab = { a: '1', b: '2' };
 const ba = { b: '2', a: '1' };
-
 assertEqual(eqObjects(ab, ba), true);
+
+// Test 2
 console.log('ab vs abc:')
 const abc = { a: '1', b: '2', c: '3' };
 assertEqual(eqObjects(ab, abc), false);
+
+// Test 3
 console.log('cd vs dc:')
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+assertEqual(eqObjects(cd, dc), true); 
+
+// Test 4
 console.log('cd vs cd2:')
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);; // => false
+assertEqual(eqObjects(cd, cd2), false);; 
