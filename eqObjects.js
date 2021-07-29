ae5e4c42821e3b3107f261945d07cd51136791f3
Kelsey Erickson <kerickson@gmx.com>
2021-07-29 01:36:09 +0000

    Added test code

--- eqObjects.js
+++ eqObjects.js
@@ -10,5 +10,13 @@ const assertEqual = function(actual, expected) {
 // Otherwise returns false
 
 const eqObjects = function(object1, object2) {
-  
+
 };
+
+const ab = { a: '1', b: '2'};
+const ba = { b: '2', a: '1'};
+
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: '1', b: '2', c: '3'}
+assertEqual(eqObjects(ab, abc), false);
