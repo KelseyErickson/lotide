c022f20115a043c51bd81ce347bcf720077d96c5
Kelsey Erickson <kerickson@gmx.com>
2021-07-29 02:02:49 +0000

    added for and if statement to compare key values

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -9,18 +9,25 @@ const assertEqual = function(actual, expected) {
 // function that returns true if both objects have identical keys with identical values
 // Otherwise returns false
 
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length === Object.keys(object2).length){
-   
-  }
+const eqObjects = function (object1, object2) {
+  const keysObject1 = Object.keys(object1);
+  const keysObject2 = Object.keys(object1);
+  let result = false;
 
-  
+  if (keysObject1.length === keysObject2.length) {
+    for (const key in object1) {
+      if (object1[key] === object2[key]) {
+        result = true;
+      }
+    }
+  } 
+ return result;
 };
 
-const ab = { a: '1', b: '2'};
-const ba = { b: '2', a: '1'};
+const ab = { a: '1', b: '2' };
+const ba = { b: '2', a: '1' };
 
 assertEqual(eqObjects(ab, ba), true);
 
-const abc = { a: '1', b: '2', c: '3'};
+const abc = { a: '1', b: '2', c: '3' };
 assertEqual(eqObjects(ab, abc), false);
