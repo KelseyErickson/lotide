50b792e216b47e3499b20004939e0cb784b84272
Kelsey Erickson <kerickson@gmx.com>
2021-07-29 21:40:32 +0000

    Added inspect function to display objects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -39,11 +39,12 @@ const eqObjects = function (object1, object2) {
 // Function to take two objects and log a message to assert if the objects are equal
 
 const assertObjectsEqual = function(actual, expected) {
-  
+  const inspect = require('util').inspect;
+
   if (eqObjects(actual, expected) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else if (eqArrays(actual, expected) === false) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 
 };
