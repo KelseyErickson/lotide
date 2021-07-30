c730a38a68e82c3e8e3aed87c85e8876fca9e509
Kelsey Erickson <kerickson@gmx.com>
2021-07-30 22:35:58 +0000

    Added initial code and function definition

---
+++ map.js
@@ -0,0 +1,9 @@
+// Map function will an array to map and a callback function
+const words = ['ground', 'control', 'to', 'major', 'tom'];
+
+const map = function(){
+  
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
