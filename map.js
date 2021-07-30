c6b400c7ce63dc55a3ee3d2ed3d1e55289384b1f
Kelsey Erickson <kerickson@gmx.com>
2021-07-30 22:41:06 +0000

    Added for of loop to iterate through array

--- map.js
+++ map.js
@@ -1,8 +1,15 @@
 // Map function will an array to map and a callback function
 const words = ['ground', 'control', 'to', 'major', 'tom'];
 
-const map = function(){
-  
+const map = function(array, callback){
+  const results = [];
+
+  for (let item of array){
+    console.log('item BEFORE ', item);
+    console.log('item AFTER ', callback(item));
+    console.log('---');
+  }
+  return results;
 }
 
 const results1 = map(words, word => word[0]);
