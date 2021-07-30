084f7619cd22346bb1ab9547cb4cd7994b7d22ba
Kelsey Erickson <kerickson@gmx.com>
2021-07-30 22:51:17 +0000

    Added assertArraysEqual function

--- map.js
+++ map.js
@@ -1,5 +1,31 @@
-// Map function will an array to map and a callback function
-const words = ['ground', 'control', 'to', 'major', 'tom'];
+onst eqArrays = function(array1, array2) {
+  let outcome = true;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1.length === array2.length) {
+      if (array1[i] !== array2[i]) {
+        outcome = false;
+      }
+    } else {
+      outcome = false;
+    }
+  }
+  return outcome;
+};
+
+// Function that uses eqArrays to print a message indicating if two arrays are equal or not
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else if (eqArrays(array1, array2) === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
+
+
+// Function will take an array to map and a callback function
 
 const map = function(array, callback){
   const results = [];
@@ -10,5 +36,6 @@ const map = function(array, callback){
   return results;
 }
 
+const words = ['ground', 'control', 'to', 'major', 'tom'];
 const results1 = map(words, word => word[0]);
 console.log(results1);
