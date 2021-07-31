031dfbbb015a36e80bcd68ab572fe92d854d27b4
Kelsey Erickson <kerickson@gmx.com>
2021-07-31 16:04:00 +0000

    Added first test case

--- findkey.js
+++ findkey.js
@@ -13,3 +13,16 @@ const assertEqual = function(actual, expected) {
 const findkey = function(object, callback) {
 
 };
+
+
+// Test One
+
+assertEqual(findKey({
+  'Blue Hill': { stars: 1 },
+  'Akaleri':   { stars: 3 },
+  'noma':      { stars: 2 },
+  'elBulli':   { stars: 3 },
+  'Ora':       { stars: 2 },
+  'Akelarre':  { stars: 3 }
+}, x => x.stars === 2), 'noma') // => True
+
