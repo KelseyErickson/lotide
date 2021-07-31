235fd6df148a463ec5d50a7210944c6d8d219b41
Kelsey Erickson <kerickson@gmx.com>
2021-07-31 15:51:52 +0000

    Added test cases and fixed eslint errors

--- takeUntil.js
+++ takeUntil.js
@@ -27,7 +27,7 @@ const assertArraysEqual = function(array1, array2) {
 
 
 const takeUntil = function(array, callback) {
-  for (element of array){
+  for (const element of array) {
     if (callback(element)) {
       return array.slice(0, array.indexOf(element));
     }
@@ -36,9 +36,17 @@ const takeUntil = function(array, callback) {
 
 // Test one
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]); // Pass
 
 // Test two
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
+const data2 = ['I\'ve', 'been', 'to', 'Hollywood', ',', 'I\'ve', 'been', 'to', 'Redwood'];
+assertArraysEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']); // Pass
+
+// Test three
+const data3 = [27, 'Hello', false, false, true, 'Potato'];
+assertArraysEqual(takeUntil(data3, x => x === true), [27, 'Hello', false, false]); // Pass
+
+// Test three
+const data4 = [1, 1, 1, 1, 1, 15, 1, 1, 1];
+assertArraysEqual(takeUntil(data4, x => x !== 1), [1, 1, 1, 1, 1]); // Pass
 
