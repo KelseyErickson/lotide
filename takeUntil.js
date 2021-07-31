9cf9650b3c25dc852f3366f5e8c73c5eec69e99b
Kelsey Erickson <kerickson@gmx.com>
2021-07-31 00:44:05 +0000

    Added initial function definition and loop to iterate through array

---
+++ takeUntil.js
@@ -0,0 +1,10 @@
+// Function will take in two parameters - an array and a callback function
+// Return a slice of the array with elements taken from the beginning an dwill continue untile the callback returns a truthy value
+
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (element of array){
+    
+  }
+};
