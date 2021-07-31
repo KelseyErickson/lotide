d22faca0a34343742b37f6e20c463a96e43f8b67
Kelsey Erickson <kerickson@gmx.com>
2021-07-31 16:12:01 +0000

    Added for and if loop to find the matching key and added new test case

--- findkey.js
+++ findkey.js
@@ -10,8 +10,13 @@ const assertEqual = function(actual, expected) {
 // It will scan the object and return the first key for which the call back returns a truthy value
 // If no key is found it will return undefined
 
-const findkey = function(object, callback) {
-
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if(callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined
 };
 
 
@@ -26,3 +31,13 @@ assertEqual(findKey({
   'Akelarre':  { stars: 3 }
 }, x => x.stars === 2), 'noma') // => True
 
+//Test Two
+
+assertEqual(findKey({
+  'Blue Hill': { stars: 1 },
+  'Akaleri':   { stars: 3 },
+  'noma':      { stars: 2 },
+  'elBulli':   { stars: 3 },
+  'Ora':       { stars: 2 },
+  'Akelarre':  { stars: 3 }
+}, x => x.stars === 4), undefined) // => True
