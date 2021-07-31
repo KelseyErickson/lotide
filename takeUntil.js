723ec32eae170759020e92de99432d2744f7ac4c
Kelsey Erickson <kerickson@gmx.com>
2021-07-31 15:40:21 +0000

    Refactored code and added assertArraysEqual to test code

--- takeUntil.js
+++ takeUntil.js
@@ -32,13 +32,13 @@ const takeUntil = function(array, callback) {
       return array.slice(0, array.indexOf(element));
     }
   }
-  
 };
 
-//Test one
+// Test one
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
 
+// Test two
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
 
