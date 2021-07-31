b39bbe68dae8bd444f98ea3ce7bc03b16a1a6cee
Kelsey Erickson <kerickson@gmx.com>
2021-07-31 01:12:38 +0000

    Removed unneeded variable and added test code

--- takeUntil.js
+++ takeUntil.js
@@ -27,21 +27,18 @@ const assertArraysEqual = function(array1, array2) {
 
 
 const takeUntil = function(array, callback) {
-  let results = [];
   for (element of array){
     if (callback(element)) {
-      results = array.slice(0, array.indexOf(element));
+      return array.slice(0, array.indexOf(element));
     }
   }
-  return results;
+  
 };
 
+//Test one
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", "been", "to", "Hollywood"]);
+
