68045f00f4003a3abcea02235fa166b18c481721
Kelsey Erickson <kerickson@gmx.com>
2021-07-31 01:06:32 +0000

    Added assertArraysEqual for test cases

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,27 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+    
+  }
+  return true;
+};
+
+// Function that uses eqArrays to print a message indicating if two arrays are equal or not
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else if (eqArrays(array1, array2) === false) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+
 // Function will take in two parameters - an array and a callback function
 // Return a slice of the array with elements taken from the beginning and will continue until the callback returns a truthy value
 
