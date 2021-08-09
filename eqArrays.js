ca5163634f33cb21c6c07336893417867455db8a
Kelsey Erickson <kerickson@gmx.com>
2021-08-09 17:25:10 +0000

    Fixed eslint errors

--- eqArrays.js
+++ eqArrays.js
@@ -13,8 +13,8 @@ const eqArrays = function(array1, array2) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if(Array.isArray(array1[i])){
-      if(!eqArrays(array1[i], array2[i])){
+    if (Array.isArray(array1[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
         return false;
       }
     } else if (array1[i] !== array2[i]) {
@@ -26,17 +26,17 @@ const eqArrays = function(array1, array2) {
 };
 
 // Un-nested Array Tests
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 4, 3], [1, 2, 3]), false); 
-assertEqual(eqArrays([1, 4, 3], [1, 2, '3']), false); 
-assertEqual(eqArrays(['hello', 'goodbye', false, 27], ['hello', 'goodbye', false, 27]), true); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 4, 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 4, 3], [1, 2, '3']), false);
+assertEqual(eqArrays(['hello', 'goodbye', false, 27], ['hello', 'goodbye', false, 27]), true);
 assertEqual(eqArrays([], []), true);
 
 // Nested Array Tests
-assertEqual(eqArrays([1, [2], 3], [1, [2], 3]), true); 
-assertEqual(eqArrays([1, [2,['A']], 3], [1, [2, ['A']], 3]), true); 
-assertEqual(eqArrays([1, [2,['A']], 3], [1, [2, ['A']], 3]), true); 
-assertEqual(eqArrays([1, [2, ['A']], 3], [1, [2], 3]), false); 
-assertEqual(eqArrays([1, [2, [3], 4],[2 ['A',['Hello']]], 3], [1, [2, [3], 4],[2 ['A',['Hello']]], 3]), true); 
-assertEqual(eqArrays([1, [2, [3], 4],[2 ['A',['Hello']]], 3], [1, [2, [3], 4],[2, ['A']], 3]), false); 
-assertEqual(eqArrays([1, [2, [3], 4],[2 ['A',['Hello', ['Goodbye']]]], 3], [1, [2, [3], 4],[2 ['A',['Hello', ['Goodbye']]]], 3]), true); 
+assertEqual(eqArrays([1, [2], 3], [1, [2], 3]), true);
+assertEqual(eqArrays([1, [2,['A']], 3], [1, [2, ['A']], 3]), true);
+assertEqual(eqArrays([1, [2,['A']], 3], [1, [2, ['A']], 3]), true);
+assertEqual(eqArrays([1, [2, ['A']], 3], [1, [2], 3]), false);
+assertEqual(eqArrays([1, [2, [3], 4],[2 ['A',['Hello']]], 3], [1, [2, [3], 4],[2 ['A',['Hello']]], 3]), true);
+assertEqual(eqArrays([1, [2, [3], 4],[2 ['A',['Hello']]], 3], [1, [2, [3], 4],[2, ['A']], 3]), false);
+assertEqual(eqArrays([1, [2, [3], 4],[2 ['A',['Hello', ['Goodbye']]]], 3], [1, [2, [3], 4],[2 ['A',['Hello', ['Goodbye']]]], 3]), true);
