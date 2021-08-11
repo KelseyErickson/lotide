eeca39f0c8606d938cd5c63074f3f137bcc4b53d
Kelsey Erickson <kerickson@gmx.com>
2021-08-11 20:00:05 +0000

    Added mocha and chai to test files

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,20 +1,64 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
+
+
+describe('#middle', () => {
+
+  // Odd cases
+  it('returns [2] for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+
+  });
+
+  it('returns [3] for [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+  });
+
+  // Even Cases
+  it('returns [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+
+  });
+
+  it('returns [3, 4] for [1, 2, 3, 4, 5, 6]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+  });
+
+  // Arrays 2 or less
+  it('returns [] for [1]', () => {
+    assert.deepEqual(middle([1]), []);
+
+  });
+
+  it('returns [] for [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), []);
+
+  });
+
+  // Strings
+  it('returns ["Hello"] for ["Testing", "Hello", "Test"]', () => {
+    assert.deepEqual(middle(["Testing", "Hello", "Test"]), ["Hello"]);
+
+  });
+
+  it('returns ["Hello", "Test"] for ["Testing", "Hello", "Test", "Goodbye"]', () => {
+    assert.deepEqual(middle(["Testing", "Hello", "Test", "Goodbye"]), ["Hello", "Test"]);
+
+  });
+
+
+
+
+
+
+});
 
 // Test Cases
 
-// Odd Cases
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 
-// Even Cases
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 
-// When array is < 2
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-// Strings
-assertArraysEqual(middle(['Testing', 'Hello', 'Test']), ['Hello']);
-assertArraysEqual(middle(['Testing', 'Hello', 'Test', 'Goodbye']), ['Hello', 'Test']);
+// // Strings
+// assertArraysEqual(middle(['Testing', 'Hello', 'Test']), ['Hello']);
+// assertArraysEqual(middle(['Testing', 'Hello', 'Test', 'Goodbye']), ['Hello', 'Test']);
